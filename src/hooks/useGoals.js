ce75fd944de89233ac70ea87594cd062eb015a53
Your Name <caleb.muindi@student.moringaschool.com>
2025-07-24 22:22:20 +0300

    Fix package.json and add deployment config

--- src/hooks/useGoals.js
+++ src/hooks/useGoals.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
-import { getGoals, addGoal, updateGoal, deleteGoal } from '../api';
+// eslint-disable-next-line no-unused-vars
+import { getGoals, addGoal, updateGoal, deleteGoal } from '../api'; 
 
 export default function useGoals() {
   const [goals, setGoals] = useState([]);
